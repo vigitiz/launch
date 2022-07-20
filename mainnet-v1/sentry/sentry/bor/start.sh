100e457932c6ed818b6cc8b6ff9c415a2c0a8887
vishwajeety123 <vishwajeet@eiysys.in>
2022-07-20 13:14:37 +0530

    remove testnet

--- mainnet-v1/sentry/sentry/bor/start.sh
+++ mainnet-v1/sentry/sentry/bor/start.sh
@@ -7,24 +7,23 @@ DATA_DIR=$BOR_DIR/data
 
 bor --datadir $DATA_DIR \
   --port 30303 \
-  --http --http.addr '127.0.0.1' \
+  --http --http.addr '0.0.0.0' \
   --http.vhosts '*' \
   --http.corsdomain '*' \
   --http.port 8545 \
   --ipcpath $DATA_DIR/bor.ipc \
   --http.api 'eth,net,web3,txpool,bor' \
   --syncmode 'full' \
-  --networkid '137' \
-  --miner.gasprice '30000000000' \
+  --networkid '7352' \
   --miner.gaslimit '20000000' \
   --miner.gastarget '20000000' \
   --txpool.nolocals \
   --txpool.accountslots 16 \
-  --txpool.globalslots 32768 \
-  --txpool.accountqueue 16 \
-  --txpool.globalqueue 32768 \
-  --txpool.pricelimit '30000000000' \
+  --txpool.globalslots 131072 \
+  --txpool.accountqueue 64 \
+  --txpool.globalqueue 131072 \
   --txpool.lifetime '1h30m0s' \
   --maxpeers 200 \
   --metrics \
   --pprof --pprof.port 7071 --pprof.addr '0.0.0.0'
+  --bootnodes "enode://fa05f4874f2b0dc79f82b8d0f8795363ec44d47f3f92f031e12e76b1a62bf5d64f085051f9e968c6e9263f3bcdd0e15dd6e6742707e91a5293da00f13e018417@10.0.0.87:30303"
--- mainnet-v1/sentry/validator/bor/start.sh
+++ mainnet-v1/sentry/validator/bor/start.sh
@@ -14,23 +14,21 @@ DATA_DIR=$BOR_DIR/data
 
 bor --datadir $DATA_DIR \
   --port 30303 \
-  --http --http.addr '127.0.0.1' \
+  --http --http.addr '0.0.0.0' \
   --http.vhosts '*' \
   --http.corsdomain '*' \
   --http.port 8545 \
   --ipcpath $DATA_DIR/bor.ipc \
   --http.api 'eth,net,web3,txpool,bor' \
-  --networkid '137' \
+  --networkid '7352' \
   --syncmode 'full' \
-  --miner.gasprice '30000000000' \
   --miner.gaslimit '20000000' \
   --miner.gastarget '20000000' \
   --txpool.nolocals \
   --txpool.accountslots 16 \
-  --txpool.globalslots 32768 \
-  --txpool.accountqueue 16 \
-  --txpool.globalqueue 32768 \
-  --txpool.pricelimit '30000000000' \
+  --txpool.globalslots 131072 \
+  --txpool.accountqueue 64 \
+  --txpool.globalqueue 131072 \
   --txpool.lifetime '1h30m0s' \
   --keystore $BOR_DIR/keystore \
   --unlock $ADDRESS \
