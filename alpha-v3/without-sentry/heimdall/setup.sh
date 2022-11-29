5e5159ecadd6595af4def0b24fa0fdfceacd476d
vikas <37859740+vikasmsg@users.noreply.github.com>
2022-11-29 14:30:54 +0530

    Update setup.sh

--- alpha-v3/without-sentry/heimdall/setup.sh
+++ alpha-v3/without-sentry/heimdall/setup.sh
@@ -7,11 +7,10 @@ HEIMDALL_HOME=/data/heimdalld
 sudo mkdir -p $HEIMDALL_HOME/data
 sudo mkdir -p $HEIMDALL_HOME/config
 
-sudo chown -R ubuntu:ubuntu $HEIMDALL_HOME/data 
-sudo chown -R ubuntu:ubuntu $HEIMDALL_HOME/config
+sudo chown -R ubuntu:ubuntu /data
 
 # init heimdall node
-heimdalld init --id ${validator_id} --chain-id "heimdall-417" --home /data
+heimdalld init --id ${validator_id} --chain-id "heimdall-417" --home /data/heimdalld
 
 # copy node directories to home directories
 cp -rf $NODE_DIR/heimdall/config/genesis.json $HEIMDALL_HOME/config/
